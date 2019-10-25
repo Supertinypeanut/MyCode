9be02476dffbbf13b45270fe07fa8c409f03e3c1
supertinypeaunt <1176032017@qq.com>
2019-10-25 18:57:10 +0800

    回顾node静态服务器

---
+++ staticWeb__node/app.js
@@ -0,0 +1,53 @@
+// **************导入模块*********
+// 文件模块
+const fs = require('fs');
+// 路径模块
+const path = require('path');
+// http服务模块
+const http = require('http');
+
+
+// 创建http服务对象
+const server = http.createServer((request, response) => {
+
+  // 获取浏览器端请求资源的资源路径，因为但发生get请求时可能有中文，在发送给服务器时，都有进行encodeURI转码，所以我们需要进行decodeURI解码
+  let URL = path.join(__dirname, './public', decodeURI(request.url));
+  // 读取路径内容，因为我们的HTML文档有声明utf-8，基本的静态资源浏览器都可以识别
+  fs.readFile(URL, (error, data) => {
+    //   判断是否存在该请求资源
+    if (error == null) {
+      //   响应静态资源
+      response.end(data);
+    } else {
+      //   获取404页面路径
+      URL = path.join(__dirname, './public/404.html');
+      fs.readFile(URL, (error, data) => {
+        //   返回404页面
+        response.end(data);
+      });
+    }
+  });
+
+
+});
+
+// 监听服务对象，开启端口10086
+server.listen(10086, error => {
+  if (error == null) {
+    console.log('10086端口，服务开启成功了！！！');
+  } else {
+    console.log('Sorry,服务开启失败');
+  }
+});
+
+
+// *****************使用第三方模块*******************
+
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (request, response) => {
+//   res.send('Hello World')
+// })
+
+// app.listen(10086);
